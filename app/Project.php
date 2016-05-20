e572f9c5b04c2240ddff66b99810ca0b522a66e1
emerge <i.i.babko@gmail.com>
2016-05-20 23:45:12 +0300

    add relationships on models

--- app/Project.php
+++ app/Project.php
@@ -3,8 +3,18 @@
 namespace ProjectManager;
 
 use Illuminate\Database\Eloquent\Model;
+use ProjectManager\Client;
+use ProjectManager\Worker;
 
 class Project extends Model
 {
-    //
+    public function client() {
+        
+        $this->belongsTo(Client::class);
+    }
+    
+    public function workers() {
+        
+        $this->hasMany(Worker::class);
+    }
 }
--- app/Worker.php
+++ app/Worker.php
@@ -3,8 +3,12 @@
 namespace ProjectManager;
 
 use Illuminate\Database\Eloquent\Model;
+use ProjectManager\Project;
 
 class Worker extends Model
 {
-    //
+    public function project() {
+        
+        return $this->belongsTo(Project::class);
+    }
 }
