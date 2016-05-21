663bd19cf2291297b4b147bdd62d1b5715e2fd9f
emerge <i.i.babko@gmail.com>
2016-05-21 18:18:40 +0300

    create seeder for projects table

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use ProjectManager\Project;
 
-class DatabaseSeeder extends Seeder
+class ProjectsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
-        $this->call(ProjectsTableSeeder::class);
+        factory(Project::class, 20)->create();
     }
 }
