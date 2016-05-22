ac7828b9fe3fe14b80c204635f23301b3001efc2
emerge <i.i.babko@gmail.com>
2016-05-22 17:48:20 +0300

    add clients relationship to Organisation model

--- app/Organization.php
+++ app/Organization.php
@@ -3,8 +3,11 @@
 namespace ProjectManager;
 
 use Illuminate\Database\Eloquent\Model;
+use ProjectManager\Client;
 
 class Organization extends Model
 {
-    //
+    public function clients() {
+        return $this->hasMany(Client::class);
+    }
 }
