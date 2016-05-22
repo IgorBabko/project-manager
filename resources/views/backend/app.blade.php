fd0d2d665c05c60b5c76361b5d7597da8616bbcd
emerge <i.i.babko@gmail.com>
2016-05-22 20:25:37 +0300

    add styles blade partial

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -8,11 +8,12 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <title>Project Manager</title>
-        <link rel="stylesheet" href="/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/css/bootstrap-table.min.css">
-        <link rel="stylesheet" href="/css/app.css">
+        
+        @include('backend.partials.styles')
+    
     </head>
     <body>
+    
         <app>
             @include('backend.partials.loading')
         </app>
@@ -24,5 +25,6 @@
                 document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
             </script>
         @endif
+        
     </body>
 </html>
