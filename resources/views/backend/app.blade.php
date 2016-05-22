827619fb0a5e4d64fb44e575ed303a14f86ae77c
emerge <i.i.babko@gmail.com>
2016-05-22 20:27:19 +0300

    add livereload blade partial

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <base href="/"/>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
         <title>Project Manager</title>
+        <base href="/"/>        
         
         @include('backend.partials.styles')
     
@@ -20,11 +20,7 @@
 
         @include('backend.partials.scripts')
         
-        @if (Config::get('app.debug'))
-            <script type="text/javascript">
-                document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
-            </script>
-        @endif
+        @include('backend.partials.livereload')
         
     </body>
 </html>
