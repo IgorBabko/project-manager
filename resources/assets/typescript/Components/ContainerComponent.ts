8f5b77dc6d51e1c4b5fbd0c1eb26f7a6164a3332
emerge <i.i.babko@gmail.com>
2016-05-22 19:14:35 +0300

    add ContainerComponent and its template

---
+++ resources/assets/typescript/Components/ContainerComponent.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'container',
+    templateUrl: '/templates/ContainerComponent.main'
+})
+export class ContainerComponent {
+    
+}
