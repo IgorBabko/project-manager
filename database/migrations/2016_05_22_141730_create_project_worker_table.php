ba850d81c3812146531966675a594324e0241d8a
emerge <i.i.babko@gmail.com>
2016-05-22 17:22:28 +0300

    add project_worker pivot table

---
+++ database/migrations/2016_05_22_141730_create_project_worker_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectWorkerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_worker', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->integer('project_id')->unsigned()->index();
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            
+            $table->integer('worker_id')->unsigned()->index();
+            $table->foreign('worker_id')->references('id')->on('workers')->onDelete('cascade');
+            
+            $table->primary(['project_id', 'worker_id']);
+            
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('project_worker');
+    }
+}
