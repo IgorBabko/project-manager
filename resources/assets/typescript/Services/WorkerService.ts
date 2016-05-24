35d932c98f38aa677f024212c539ec0807ef2470
emerge <i.i.babko@gmail.com>
2016-05-24 17:30:49 +0300

    add WorkerService

---
+++ resources/assets/typescript/Services/WorkerService.ts
@@ -0,0 +1,31 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import { Project }        from '../Models/WorkerModel'
+import 'rxjs';
+
+@Injectable()
+export class WorkerService {
+
+    constructor(private http: Http) { }
+    
+    private workersUrl = 'workers';
+    
+    getWorkers(): Observable<Worker[]> {
+        return this.http.get(this.workersUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+    
+    private extractData(res: Response) {
+        return res.json();
+    }
+    
+    private handleError(error: any) {
+        
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+            
+        return Observable.throw(errMsg);
+    }
+}
