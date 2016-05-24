7ef16d601f82370f1b5f41c09e917cbaaba3dfe0
emerge <i.i.babko@gmail.com>
2016-05-24 17:38:52 +0300

    add ClientListComponent class

---
+++ resources/assets/typescript/Components/ClientListComponent.ts
@@ -0,0 +1,64 @@
+import { Component, Inject, Injectable, OnInit } from '@angular/core';
+import { ProjectService } from '../Services/ClientService';
+import { StatisticsComponent } from './StatisticsComponent';
+import { Router } from '@angular/router';
+
+declare var jQuery: any;
+
+@Component({
+    'templateUrl': '/templates/ClientListComponent.main',
+    providers: [ClientService],
+    directives: [StatisticsComponent]
+})
+export class ClientListComponent implements OnInit {
+
+    private clients;
+    private errorMessage;
+
+    constructor(private clientService: ClientService) {
+
+    }
+
+    ngOnInit() {
+        this.getClients();
+
+        jQuery('#table').bootstrapTable({
+            search: true,
+            pagination: true,
+            showColumns: true,
+            showToggle: true,
+            showRefresh: true,
+            cardView: true,
+            url: '/clients',
+            columns: [{
+                field: 'id',
+                title: 'ID',
+                sortable: true
+            }, {
+                field: 'firstName',
+                title: 'First name',
+                sortable: true
+            }, {
+                field: 'lastName',
+                title: 'Last name',
+                sortable: true
+            },{
+                field: 'age',
+                title: 'Age',
+                sortable: true
+            },{
+                field: 'salary',
+                title: 'Salary',
+                sortable: true
+            }]
+        });
+    }
+
+    private getClients() {
+        this.clientService
+            .getClients()
+            .subscribe(
+            clients => this.clients = clients,
+            error => this.errorMessage = <any>error);
+    }
+}
