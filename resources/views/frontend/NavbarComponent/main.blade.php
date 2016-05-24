93cf531680308077b5617641d098bdea123bcf07
emerge <i.i.babko@gmail.com>
2016-05-24 19:08:03 +0300

    change link to main page into navbar template

--- resources/views/frontend/NavbarComponent/main.blade.php
+++ resources/views/frontend/NavbarComponent/main.blade.php
@@ -115,7 +115,7 @@
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li class="active">
-                <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                <a [routerLink]="['/']"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
             </li>
             <li>
                 <a [routerLink]="['/projects']"><i class="fa fa-fw fa-bar-chart-o"></i> Projects</a>
