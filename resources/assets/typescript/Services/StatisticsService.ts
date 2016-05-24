a3ceed7cd38981624307534c5d81071e35a97a35
emerge <i.i.babko@gmail.com>
2016-05-24 22:12:15 +0300

    add statistics service

---
+++ resources/assets/typescript/Services/StatisticsService.ts
@@ -0,0 +1,31 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import { Statistics }     from '../Models/StatisticsModel';
+import 'rxjs';
+
+@Injectable()
+export class StatisticsService {
+
+    constructor(private http: Http) { }
+    
+    private statisticsUrl = '/statistics';
+    
+    getStatistics(): Observable<Statistics[]> {
+        return this.http.get(this.statisticsUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+    
+    private extractData(res: Response) {
+        return res.json();
+    }
+    
+    private handleError(error: any) {
+        
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+            
+        return Observable.throw(errMsg);
+    }
+}
