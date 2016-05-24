efd12d5106f5c8462dc15dd1f7d4a10ba9a8c8c5
emerge <i.i.babko@gmail.com>
2016-05-24 17:34:24 +0300

    add WorkerModel

---
+++ resources/assets/typescript/Models/WorkerModel.ts
@@ -0,0 +1,10 @@
+export class Project {
+    
+    constructor(
+        private firstName: string,
+        private lastName: string,
+        private age: number,
+        private salary: number
+    ) {}
+
+}
