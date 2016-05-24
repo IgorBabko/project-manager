304470a30ca501d9da5d5485d6793940053be018
emerge <i.i.babko@gmail.com>
2016-05-24 22:20:14 +0300

    add StatisticsComponent class

--- resources/assets/typescript/Components/StatisticsComponent.ts
+++ resources/assets/typescript/Components/StatisticsComponent.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core'
+import { StatisticsService } from '../Services/StatisticsService'
 
 @Component({
     selector: 'statistics',
-    templateUrl: '/templates/StatisticsComponent.main'
+    templateUrl: '/templates/StatisticsComponent.main',
+    providers: [ StatisticsService ],
+
 })
 export class StatisticsComponent {
     
+    private statistics;
+    private errorMessage;
+
+    constructor(private statisticsService: StatisticsService) {
+
+    }
+
+    ngOnInit() {
+    }
+
+    private getStatistics() {
+        this.statisticsService
+            .getStatistics()
+            .subscribe(
+                statistics => this.statistics = statistics,
+                error => this.errorMessage = <any>error
+            );
+    }
 }
