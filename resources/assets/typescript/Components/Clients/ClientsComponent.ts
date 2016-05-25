ff6784c552f408d65984181bd747b849155488d1
emerge <i.i.babko@gmail.com>
2016-05-25 15:01:19 +0300

    add create template for client model

--- resources/assets/typescript/Components/Clients/ClientsComponent.ts
+++ resources/assets/typescript/Components/Clients/ClientsComponent.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TableComponent } from './TableComponent';
+import { CreateComponent } from './CreateComponent';
+import { BaseComponent } from '../../Shared/BaseComponent';
 import { Routes, ROUTER_DIRECTIVES } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -9,19 +11,19 @@ declare var jQuery: any;
     {
         path: '/',
         component: TableComponent
+    },
+    {
+        path: '/create',
+        component: CreateComponent
     }
-    // {
-    //     path: '/create',
-    //     component: CreateComponent
-    // }
 ])
 @Component({
     'templateUrl': '/templates/clients.index',
     directives: [ ROUTER_DIRECTIVES ]
 })
-export class ClientsComponent {
+export class ClientsComponent extends BaseComponent {
     
-    constructor(private location: Location) {
+    constructor(location: Location) {
         super(location);
     }
 }
