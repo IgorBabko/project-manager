e20f122ab8ad6370f009dd33356cb06d14ac7413
emerge <i.i.babko@gmail.com>
2016-05-25 08:49:32 +0300

    add styles to loading icon

--- resources/views/frontend/ProjectListComponent/main.blade.php
+++ resources/views/frontend/ProjectListComponent/main.blade.php
@@ -7,16 +7,16 @@
                 <h1 class="page-header">
                     Dashboard <small>overview</small>
                 </h1>
-                <ol class="breadcrumb">
+                <!--<ol class="breadcrumb">
                     <li class="active">
                         <i class="fa fa-dashboard"></i> Dashboard
                     </li>
-                </ol>
+                </ol>-->
             </div>
         </div>
         
         <div class="row">
-            <i *ngIf="!isLoaded" class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+            <i *ngIf="!isLoaded"  class="fa fa-spinner fa-spin fa-5x fa-fw loader-icon"></i>
             <table id="table"></table>
         </div>
     </div>
