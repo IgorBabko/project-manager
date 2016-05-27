8cfebd942d4c5a1f377821d39017aca27c8e561a
emerge <i.i.babko@gmail.com>
2016-05-27 12:31:04 +0300

    change worker model

--- resources/assets/typescript/Models/WorkerModel.ts
+++ resources/assets/typescript/Models/WorkerModel.ts
@@ -1,10 +1,20 @@
 export class Worker {
+     
+    private firstName: string;
+    private lastName: string;
+    private age: number|string;
+    private salary: number|string;
     
     constructor(
-        private firstName: string,
-        private lastName: string,
-        private age: number,
-        private salary: number
-    ) {}
+        firstName?: string,
+        lastName?: string,
+        age?: number|string,
+        salary?: number|string
+    ) {
+        this.firstName = firstName || '';
+        this.lastName = lastName || '';
+        this.age = age || '';
+        this.salary = salary || '';
+    }
 
 }
