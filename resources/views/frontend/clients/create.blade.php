1f1f3e10d2dec7688f3b26f8b456c2b7f4e64747
emerge <i.i.babko@gmail.com>
2016-05-27 14:56:05 +0300

    delete organisation_id field from the create client form

--- resources/views/frontend/clients/create.blade.php
+++ resources/views/frontend/clients/create.blade.php
@@ -8,11 +8,6 @@
             <label for="last_name">Last name</label>
             <input type="text" class="form-control" id="last_name" placeholder="Last name" [(ngModel)]="client['last_name']">
         </div>
-        <div class="form-group">
-            <label for="organisation_id">Organisation ID</label>
-            <input type="text" class="form-control" id="organisation_id" placeholder="organisation_id" [(ngModel)]="client['organisation_id']">
-        </div>
-       
         <a [routerLink]="['/clients']" class="btn btn-default">Cancel</a>
         <button type="submit" class="btn btn-success">Create</button>
         <i *ngIf="isLoading" class="fa fa-spinner fa-spin fa-fw" style="margin-left: 10px"></i>
