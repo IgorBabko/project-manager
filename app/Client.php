96d74015cac232a1cfec5e854682edcdf3263aa0
emerge <i.i.babko@gmail.com>
2016-05-27 15:00:16 +0300

    allow all fields to be mass assigned on Project and Client models

--- app/Client.php
+++ app/Client.php
@@ -7,6 +7,9 @@ use ProjectManager\Project;
 
 class Client extends Model
 {
+    protected $guarded = [];
+
+    
     public function projects() {
         
         return $this->hasMany(Project::class);
--- app/Worker.php
+++ app/Worker.php
@@ -7,6 +7,8 @@ use ProjectManager\Project;
 
 class Worker extends Model
 {
+    protected $guarded = [];
+    
     public function projects() {
         
         return $this->belongsToMany(Project::class);
