3fdc44706d0034250be3dd5ca9595151ba9c8803
emerge <i.i.babko@gmail.com>
2016-05-31 09:18:01 +0300

    add getProjects method, declare jQuery, inject UtilService in CreateComponent for clients

--- resources/assets/typescript/Components/Clients/CreateComponent.ts
+++ resources/assets/typescript/Components/Clients/CreateComponent.ts
@@ -1,24 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../Services/ClientService';
 import { ProjectService } from '../../Services/ProjectService';
+import { UtilService } from '../../Services/UtilService';
 import { Client } from '../../Models/ClientModel';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router';
+
+declare var jQuery: any;
 
 @Component({
     templateUrl: '/templates/clients.create',
     providers: [ ClientService, ProjectService ],
     directives: [ ROUTER_DIRECTIVES ]
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
     
     private client: Client = new Client();
     private isLoading = false;
+    private errorMessage;
     
     constructor(
         private clientService: ClientService,
         private projectService: ProjectService,
+        private utilService: UtilService,
         private router: Router
     ) {}
+    
+    public ngOnInit() {
+        this.getProjects();
+    }
+    
+    public getProjects() {
+        this.projectService
+            .getProjects()
+            .subscribe(
+                projects => {
+                    this.utilService.buildSelectList(
+                        jQuery('select.projects'), projects
+                    );
+                },
+                error => this.errorMessage = <any>error
+            );
+    }
     
     public postClient() {
         this.clientService
