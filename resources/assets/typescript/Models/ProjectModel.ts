962e42f0f2751aa6cb56bfa7b0a8eefd7538ba68
emerge <i.i.babko@gmail.com>
2016-05-31 07:21:32 +0300

    add client_id property on ProjectModel.ts

--- resources/assets/typescript/Models/ProjectModel.ts
+++ resources/assets/typescript/Models/ProjectModel.ts
@@ -1,6 +1,7 @@
 export class Project {
      
     private name: string;
+    private client_id: number|string;
     private description: string;
     private budget: number|string;
     private workerIds: number[];
@@ -8,12 +9,14 @@ export class Project {
     
     constructor(
         name?: string,
+        client_id?: number|string,
         description?: string,
         budget?: number,
         workerIds?: number[]
 
     ) {
         this.name = name || '';
+        this.client_id = client_id || '';
         this.description = description || '';
         this.budget = budget || '';
         this.workerIds = workerIds || [];
