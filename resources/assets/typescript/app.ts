5fb58b2cdb532d25d2ff5ee74c30e41c35d1d735
emerge <i.i.babko@gmail.com>
2016-05-31 08:25:38 +0300

    add UtilService to app.ts component

--- resources/assets/typescript/app.ts
+++ resources/assets/typescript/app.ts
@@ -3,8 +3,9 @@ import { ROUTER_PROVIDERS } from '@angular/router';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { UtilService } from './Services/UtilService';
 
 //enableProdMode();
 bootstrap(<Function>AppComponent, [
-    ROUTER_PROVIDERS, HTTP_PROVIDERS
+    ROUTER_PROVIDERS, HTTP_PROVIDERS, UtilService
 ]);
