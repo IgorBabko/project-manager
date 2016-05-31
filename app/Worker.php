de501fc3e357001e20ec565676545b815ab36c91
emerge <i.i.babko@gmail.com>
2016-05-31 14:16:21 +0300

    add withTimestamps() call to projects relationship on Worker model

--- app/Worker.php
+++ app/Worker.php
@@ -11,6 +11,6 @@ class Worker extends Model
     
     public function projects() {
         
-        return $this->belongsToMany(Project::class);
+        return $this->belongsToMany(Project::class)->withTimestamps();
     }
 }
